4baf2c44d3a92fc4f123eaa69f0b96ef711e4562
IssieJ <s2175119@ed.ac.uk>
2025-02-13 13:27:31 +0000

    edits to the random number generation code so its organised

---
+++ quadrat_data_code/pre-collection RNG.R
@@ -0,0 +1,12 @@
+# RANDOM NUMBER GENERATION 
+
+# data collection - June 04, 2024. 
+
+
+# generate number of seconds to turn for - between 5 and 15. 
+(random_integer_secs <- sample(5:15, 1))
+
+
+# generate number of steps to take - between 1 and 10. 
+(random_integer_steps <- sample(1:10, 1)) 
+
